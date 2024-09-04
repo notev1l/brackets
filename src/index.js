9d7ec896cbb8e6d4913b70d2f8be5dd5ac852aff
Viktor <skyba.viktor2016@gmail.com>
2024-09-04 22:27:11 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  console.log(str.split(''));
+
+  let openBrackets = [];
+  let closedBrackets = {}
+
+  bracketsConfig.forEach(element => {
+      openBrackets.push(element[0]);
+      closedBrackets[element[1]] = element[0];
+  })
+
+  let stack = [];
+  str.split('').forEach((element, index) => {
+    stack.push(element);
+    if (stack[stack.length-2] !== undefined) {
+      if (closedBrackets[stack[stack.length-1]] === stack[stack.length-2]) {
+        stack.pop();
+        stack.pop();
+      }
+    }
+  })
+  if (stack.length === 0) {
+      return true
+  } else {
+      return false
+  }
 }
